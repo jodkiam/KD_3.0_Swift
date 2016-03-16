3172cba4d193067355776ee5c0e23c704da97977
jodkiam <kdnet@kdnetdeimac.local>
2016-03-16 10:37:16 +0800

    Initial Commit

---
+++ KD_3.0_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KD_3.0_Swift
+//
+//  Created by kdnet on 16/3/16.
+//  Copyright © 2016年 kdnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
